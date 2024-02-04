5a1a6b6c9251393d8a56d22e5c204d4f35cbb970
chaindev <chaindev@icloud.com>
2024-02-04 13:42:00 +0100

    fix

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -25,31 +25,21 @@ export default function Home() {
                             </div>
                         </div>
                     </div>
-                    <div className="tf-section-2 contact-us">
-                        <div className="themesflat-container">
-                            <div className="row">
-                                <div className="col-12">
-                                    <div className="widget-gg-map">
-                                        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d97101.88872869895!2d-74.22688511715344!3d40.487336736141906!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1svi!2s!4v1689125037376!5m2!1svi!2s" width="100%" height={460} style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                   
                     <div className="tf-section-2 widget-box-icon">
                         <div className="themesflat-container">
                             <div className="row">
                                 <div className="col-md-12">
                                     <div className="heading-section-1">
                                         <h2 className="tf-title pb-20">Information</h2>
-                                        <p className="pb-40">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                                        <p className="pb-40">Welcome to Blockcity the Stacks-powered digital asset marketplace supporting Ordinals, BRC-20, and SIP009 standards. Trade, create, and innovate with us! 🚀🌐</p>
                                     </div>
                                 </div>
                                 <div data-wow-delay="0s" className="wow fadeInUp col-md-4">
                                     <div className="box-icon-item">
                                         <img src="/assets/images/box-icon/address.png" alt="" />
                                         <div className="title"><Link href="#">Office address</Link></div>
-                                        <p>1901 Thornridge Cir. Shiloh, Hawaii 81063</p>
+                                        <p>Worldwide</p>
                                     </div>
                                 </div>
                                 <div data-wow-delay="0.1s" className="wow fadeInUp col-md-4">
@@ -64,8 +54,8 @@ export default function Home() {
                                     <div className="box-icon-item">
                                         <img src="/assets/images/box-icon/phone.png" alt="" />
                                         <div className="title"><Link href="#">Phone number</Link></div>
-                                        <p>(316) 555-0116 <br />
-                                            (219) 555-0114</p>
+                                        <p>+385995217130 <br />
+                                            +152637473222</p>
                                     </div>
                                 </div>
                             </div>
