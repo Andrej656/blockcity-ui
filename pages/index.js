65b5193c36ede689e55058fee80f79602c6f860e
chaindev <chaindev@icloud.com>
2024-02-09 22:23:01 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
 import { useEffect, useState } from 'react';
 import Layout from "@/components/layout/Layout";
-import FlatTitle1 from "@/components/sections/FlatTitle1";
-import FeaturedItem1 from "@/components/sections/FeaturedItem1";
-import Seller1 from "@/components/sections/Seller1";
-import DiscoverItem1 from "@/components/sections/DiscoverItem1";
-import TopCollector1 from "@/components/sections/TopCollector1";
-import TopCollections1 from "@/components/sections/TopCollections1";
-import CreateSell1 from "@/components/sections/CreateSell1";
-import Action1 from "@/components/sections/Action1";
+
+
 
 export default function Home() {
   const principalAddress = 'ST000000000000000000002AMW42H';
@@ -47,14 +41,7 @@ export default function Home() {
   return (
     <>
       <Layout headerStyle={1} footerStyle={1} pageCls="slider-sroll">
-        <FlatTitle1 />
-        <FeaturedItem1 />
-        <Seller1 />
-        <DiscoverItem1 />
-        <TopCollector1 />
-        <TopCollections1 />
-        <CreateSell1 />
-        <Action1 />
+     
 
         {/* Render NFT Holdings */}
         <section>
