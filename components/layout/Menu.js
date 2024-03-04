1924de2898f2c161b02354a2e2204ddf10283599
Btcdev <chaindev@icloud.com>
2024-03-04 18:02:43 +0100

    test ui

--- components/layout/Menu.js
+++ components/layout/Menu.js
@@ -7,10 +7,14 @@ export default function Menu() {
   return (
     <ul id="menu-primary-menu" className="menu">
       <li className={`menu-item ${router.pathname === "/slider-scroll" ? "current-item" : ""}`}>
-        <Link href="/slider-scroll">Blockcity</Link>
+        <Link href="/slider-scroll">Home</Link>
       </li>
       <li className={`menu-item ${router.pathname === "/about-us" ? "current-item" : ""}`}>
         <Link href="/about-us">About us</Link>
+      </li>
+      {/* Add the Market menu item */}
+      <li className={`menu-item ${router.pathname === "/market" ? "current-item" : ""}`}>
+        <Link href="/market">Blockcity</Link>
       </li>
     </ul>
   );
