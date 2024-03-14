c1a42973d694d3a7746589e6ef0c9c44a55d476e
Btcdev <chaindev@icloud.com>
2024-03-14 14:49:03 +0100

    dashboard components

--- pages/_app.js
+++ pages/_app.js
@@ -1,32 +1,34 @@
-import Preloader from "@/components/elements/Preloader"
-import { useEffect, useState } from "react"
-// import 'swiper/css';
-// import "swiper/css/navigation";
-// import "swiper/css/pagination";
-// import 'swiper/css/free-mode';
-import AddClassBody from "@/components/elements/AddClassBody"
-import "/public/assets/css/style.css"
-import "/public/assets/css/responsive.css"
-import "/public/assets/css/popupstyle.css"
-function MyApp({ Component, pageProps }) {
+import Preloader from "@/components/elements/Preloader";
+import AddClassBody from "@/components/elements/AddClassBody";
+import { ChakraProvider } from '@chakra-ui/react'; // Import ChakraProvider
+import { useEffect, useState } from "react";
 
-    const [loading, setLoading] = useState(true)
+// CSS imports remain unchanged
+import "/public/assets/css/style.css";
+import "/public/assets/css/responsive.css";
+import "/public/assets/css/popupstyle.css";
+
+function MyApp({ Component, pageProps }) {
+    const [loading, setLoading] = useState(true);
+
     useEffect(() => {
         setTimeout(() => {
-            setLoading(false)
-        }, 1000)
+            setLoading(false);
+        }, 1000);
+    }, []);
 
-    }, [])
-    return (<>
-        {!loading ? (
-            <>
-                <AddClassBody />
-                <Component {...pageProps} />
-            </>
-        ) : (
-            <Preloader />
-        )}
-    </>)
+    return (
+        <ChakraProvider> {/* Wrap your app with ChakraProvider */}
+            {!loading ? (
+                <>
+                    <AddClassBody />
+                    <Component {...pageProps} />
+                </>
+            ) : (
+                <Preloader />
+            )}
+        </ChakraProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
